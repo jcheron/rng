0742518246f432b44db1cb0da24cc2eab2884fc8
jcheron <myaddressmail@gmail.com>
2019-05-10 13:25:44 +0200

    First commit

---
+++ app/controllers/RandomNumberGame.php
@@ -0,0 +1,32 @@
+<?php
+namespace controllers;
+ use Ubiquity\utils\http\USession;
+
+ /**
+ * Controller RandomNumberGame
+ **/
+class RandomNumberGame extends ControllerBase{
+    const SESSION_KEY="random";
+    
+	public function index(){
+	    if(USession::exists(self::SESSION_KEY)){
+	        $this->propose();
+	    }else{
+		  $this->loadView("RandomNumberGame/index.html");
+	    }
+	}
+
+	public function genere(){
+	    $number=\mt_rand(1,10);
+	    USession::set(self::SESSION_KEY, $number);
+	    $this->index();
+	}
+
+
+	public function propose(){
+		
+		$this->loadView('RandomNumberGame/propose.html');
+
+	}
+
+}
